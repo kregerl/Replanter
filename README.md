ae74069d51c18c0d99093cb21f3e77cefa65c174
kregerl <loucaskreger@gmail.com>
2021-07-28 16:40:04 -0400

    1.17.1 forge release

---
+++ README.md
@@ -0,0 +1,14 @@
+# Replanter
+Replanter is a simple client side mod that allows players to quickly replant their fully grown crops.  
+Simply right click on a fully grown crop while holding seeds and it'll be harvested and replanted instantly.  
+A list of supported crops below:  
+- Wheat
+- Carrots
+- Potatos
+- Beetroot
+- Nether Wart
+- Cocoa Beans (Must hold iron axe or above in mainhand and cocoa beans in offhand)
+
+Crops will be fortuned as long as the player is holding the tool in their mainhand and seeds in their offhand.  
+
+If you'd like me to add other crops, please suggest them in the comments or create an issue on [github](https://github.com/kregerl/Replanter)
---
+++ src/main/java/com/loucaskreger/replanter/client/EventSubscriber.java
@@ -0,0 +1,89 @@
+package com.loucaskreger.replanter.client;
+
+import com.loucaskreger.replanter.Replanter;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CocoaBlock;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.NetherWartBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.PlantType;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Replanter.MOD_ID, value = Dist.CLIENT)
+public class EventSubscriber {
+
+    private static final Minecraft mc = Minecraft.getInstance();
+
+    @SubscribeEvent
+    public static void onPlayerRightClick(final PlayerInteractEvent.RightClickBlock event) {
+        if (event.getSide() == LogicalSide.CLIENT) {
+            BlockHitResult result = event.getHitVec();
+            Level level = event.getWorld();
+            BlockPos pos = result.getBlockPos();
+            BlockState state = level.getBlockState(pos);
+            Block block = state.getBlock();
+            ItemStack stack = event.getItemStack();
+            Item item = stack.getItem();
+            MultiPlayerGameMode pc = mc.gameMode;
+
+            if (item instanceof BlockItem) {
+                Block itemBlock = ((BlockItem) item).getBlock();
+                if (block instanceof CropBlock) {
+
+                    if (isPlantable(itemBlock, block, level, pos)) {
+                        CropBlock cropsBlock = ((CropBlock) block);
+
+                        if (cropsBlock.isMaxAge(state)) {
+                            pc.startDestroyBlock(pos, Direction.DOWN);
+                        }
+                    }
+                } else if (block instanceof NetherWartBlock) {
+
+                    if (isPlantable(itemBlock, block, level, pos)) {
+                        NetherWartBlock wartBlock = (NetherWartBlock) block;
+                        // If nether wart block outline shape y value is max size then the block is
+                        // fully grown.
+                        if ((wartBlock.getShape(state, level, pos, CollisionContext.empty()).bounds().getYsize()
+                                * 16) == 14) {
+                            pc.startDestroyBlock(pos, Direction.DOWN);
+                        }
+                    }
+                } else if (block instanceof CocoaBlock) {
+
+                    CocoaBlock cocoaBlock = (CocoaBlock) block;
+                    if ((cocoaBlock.getShape(state, level, pos, CollisionContext.empty()).bounds()
+                            .getYsize() == 0.5625)) {
+                        pc.startDestroyBlock(pos, level.getBlockState(pos).getValue(CocoaBlock.FACING));
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean isPlantable(Block itemBlock, Block block, Level level, BlockPos pos) {
+        if (itemBlock instanceof IPlantable) {
+            PlantType itemType = ((IPlantable) itemBlock).getPlantType(level, pos);
+            PlantType blockType = ((IPlantable) block).getPlantType(level, pos);
+            if (itemType == blockType) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
