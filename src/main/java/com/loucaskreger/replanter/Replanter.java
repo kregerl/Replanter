b9235bc76675b1a61b8da1729efb93bcb0090c35
kregerl <loucaskreger@gmail.com>
2021-12-01 11:29:52 -0500

    Updated to 1.18

---
+++ src/main/java/com/loucaskreger/replanter/Replanter.java
@@ -0,0 +1,25 @@
+package com.loucaskreger.replanter;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Replanter.MOD_ID)
+public class Replanter {
+    public static final String MOD_ID = "replanter";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Replanter() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+}
