444487bbe44294a26785fd06f0b06e4e64a6c08b
kregerl <loucaskreger@gmail.com>
2023-07-30 13:27:14 -0400

    Port to 1.20.1

---
+++ src/main/java/com/loucaskreger/replanter/Replanter.java
@@ -0,0 +1,14 @@
+package com.loucaskreger.replanter;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Replanter.MODID)
+public class Replanter {
+    public static final String MODID = "replanter";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Replanter() {
+    }
+}
